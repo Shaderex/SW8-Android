ba638eeb3acf92665df136e9c833989e629e9086
unknown <nkmouritzsen@gmail.com>
2016-02-12 14:45:12 +0100

    Initialize empty AS project

---
+++ SW808F16/app/src/androidTest/java/dk/aau/sw808f16/sw808f16/ApplicationTest.java
@@ -0,0 +1,13 @@
+package dk.aau.sw808f16.sw808f16;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
