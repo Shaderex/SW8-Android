709ce33f2c5d6961427a3635a40962746e7e6ded
marhlder <marhlder@gmail.com>
2016-02-19 13:36:03 +0100

    Add AmbientLightTest.java - Adds a test which tests the ambient light sensor

---
+++ SW808F16/app/src/androidTest/java/dk/aau/sw808f16/datacollection/AmbeintLightTest.java
@@ -0,0 +1,76 @@
+package dk.aau.sw808f16.datacollection;
+
+import android.app.Application;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.test.ApplicationTestCase;
+import android.util.Log;
+
+import org.junit.Test;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * Created by Marhlder on 19-02-2016.
+ */
+public class AmbeintLightTest extends ApplicationTestCase<Application> {
+
+  public AmbeintLightTest() {
+    super(Application.class);
+  }
+
+  @Test
+  public void testAmbientLight() {
+
+    final long now = System.currentTimeMillis();
+    final long whenToStop = now + 30000;
+    final CountDownLatch latch = new CountDownLatch(1);
+
+    final Timer timer = new Timer();
+
+    final SensorManager sensorManager = (SensorManager) getContext().getSystemService(Context.SENSOR_SERVICE);
+    final Sensor lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+    final SensorEventListener listener = new SensorEventListener() {
+      @Override
+      public void onSensorChanged(final SensorEvent event) {
+
+        final float lux = event.values[0];
+
+        Log.i("LIGHT Illuminance", lux + " lx / " + lightSensor.getMaximumRange() + " lx");
+      }
+
+      @Override
+      public void onAccuracyChanged(final Sensor sensor, final int accuracy) {
+      }
+    };
+
+    sensorManager.registerListener(listener, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+    final TimerTask stopTask = new TimerTask() {
+      @Override
+      public void run() {
+        latch.countDown();
+      }
+    };
+
+    timer.schedule(stopTask, whenToStop - now);
+
+    try {
+      latch.await();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+
+    timer.cancel();
+
+    sensorManager.unregisterListener(listener);
+
+  }
+}
+
