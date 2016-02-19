2a3dfb7426173854d96b6c3a1944331952288f46
Jesper Clausen <j.clausen.1992@gmail.com>
2016-02-19 15:11:15 +0100

    Add FileToQuestionnaireParser and tests for it

---
+++ SW808F16/app/src/main/java/dk/aau/sw808f16/datacollection/questionaire/parser/FileToQuestionnaireParser.java
@@ -0,0 +1,45 @@
+package dk.aau.sw808f16.datacollection.questionaire.parser;
+
+
+import android.content.Context;
+
+import dk.aau.sw808f16.datacollection.questionaire.models.Question;
+import dk.aau.sw808f16.datacollection.questionaire.models.Questionnaire;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class FileToQuestionnaireParser {
+
+  private FileToQuestionnaireParser() {
+    throw new RuntimeException("You cannot instantiate this class");
+  }
+
+  /**
+   * Parses a file from the resources folder with the given resource identifier into a
+   * Questionnaire.
+   */
+  public static Questionnaire parseFile(Context context, int resourceId) throws IOException {
+    InputStreamReader streamReader =
+        new InputStreamReader(context.getResources().openRawResource(resourceId));
+
+    BufferedReader reader = new BufferedReader(streamReader);
+
+    List<Question> questions = new ArrayList<>();
+    String line;
+
+    while ((line = reader.readLine()) != null) {
+      Question question = new Question(line);
+      questions.add(question);
+    }
+
+    streamReader.close();
+    reader.close();
+
+    return new Questionnaire(questions);
+  }
+}
