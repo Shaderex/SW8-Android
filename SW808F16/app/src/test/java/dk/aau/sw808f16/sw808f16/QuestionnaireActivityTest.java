534f3672ef4cde944638ec281dcb08b7e3ae9788
Rasmus Holm Jensen <rhje12@student.aau.dk>
2016-02-22 09:34:15 +0100

    Create QuestionnaireActivityTest class

---
+++ SW808F16/app/src/test/java/dk/aau/sw808f16/sw808f16/QuestionnaireActivityTest.java
@@ -0,0 +1,10 @@
+package dk.aau.sw808f16.sw808f16;
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
+public class QuestionnaireActivityTest {
+}
