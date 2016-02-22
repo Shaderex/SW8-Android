1f14f9283a3b1ed1fa5a81d745da6eef02a3d504
Rasmus Holm Jensen <rhje12@student.aau.dk>
2016-02-22 09:51:36 +0100

    Add setup method for QuestionnaireActivityTtest

--- SW808F16/app/src/test/java/dk/aau/sw808f16/datacollection/questionaire/QuestionnaireActivityTest.java
+++ SW808F16/app/src/test/java/dk/aau/sw808f16/datacollection/questionaire/QuestionnaireActivityTest.java
@@ -1,13 +1,24 @@
 package dk.aau.sw808f16.datacollection.questionaire;
 
 
+import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
 
 import dk.aau.sw808f16.datacollection.BuildConfig;
+import dk.aau.sw808f16.datacollection.QuestionnaireActivity;
 
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = 21)
 public class QuestionnaireActivityTest {
+
+  private QuestionnaireActivity questionnaireActivity;
+
+  @Before
+  public void setup() {
+    questionnaireActivity = Robolectric.buildActivity(QuestionnaireActivity.class).create().get();
+  }
+
 }
