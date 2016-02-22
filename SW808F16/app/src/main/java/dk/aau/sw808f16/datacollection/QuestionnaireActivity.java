5ecf5b807a4922606abe4a2e0d56a929adf56124
Rasmus Holm Jensen <rhje12@student.aau.dk>
2016-02-22 12:07:48 +0100

    Enforce QuestionnaireActivity is to be started with an intent containing a questionnaire.

--- SW808F16/app/src/main/java/dk/aau/sw808f16/datacollection/QuestionnaireActivity.java
+++ SW808F16/app/src/main/java/dk/aau/sw808f16/datacollection/QuestionnaireActivity.java
@@ -1,13 +1,29 @@
 package dk.aau.sw808f16.datacollection;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import dk.aau.sw808f16.datacollection.questionaire.models.Questionnaire;
 
 public class QuestionnaireActivity extends Activity {
+
+  public static final String QUESTIONNAIRE_PARCEL_IDENTIFIER = "QUESTIONNAIRE_PARCEL_IDENTIFIER";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_questionnaire);
+
+    Intent intent = getIntent();
+    Questionnaire questionnaire = intent.getParcelableExtra(QUESTIONNAIRE_PARCEL_IDENTIFIER);
+
+    if (questionnaire == null) {
+      throw new IllegalArgumentException("Illegal intent sent to activity. Questionnaire was null");
+    }
+
+    TextView questionText = (TextView) findViewById(R.id.questionnaire_question_text);
+    questionText.setText(questionnaire.getNextQuestion().getQuestion());
   }
 }
--- SW808F16/app/src/test/java/dk/aau/sw808f16/datacollection/questionaire/QuestionnaireActivityTest.java
+++ SW808F16/app/src/test/java/dk/aau/sw808f16/datacollection/questionaire/QuestionnaireActivityTest.java
@@ -1,6 +1,8 @@
 package dk.aau.sw808f16.datacollection.questionaire;
 
 
+import android.content.Intent;
+import android.widget.Button;
 import android.widget.TextView;
 
 import junit.framework.Assert;
@@ -12,41 +14,74 @@ import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import dk.aau.sw808f16.datacollection.BuildConfig;
 import dk.aau.sw808f16.datacollection.QuestionnaireActivity;
 import dk.aau.sw808f16.datacollection.R;
+import dk.aau.sw808f16.datacollection.questionaire.models.Question;
+import dk.aau.sw808f16.datacollection.questionaire.models.Questionnaire;
+
 
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = 21)
 public class QuestionnaireActivityTest {
 
   private QuestionnaireActivity questionnaireActivity;
+  private Questionnaire questionnaire;
 
   @Before
   public void setup() {
-    questionnaireActivity = Robolectric.buildActivity(QuestionnaireActivity.class).create().get();
+    List<Question> questions = new ArrayList<Question>() {
+      {
+        add(new Question("Annie are you okay?"));
+        add(new Question("Are you okay Annie?"));
+      }
+    };
+
+    questionnaire = new Questionnaire(questions);
+
+    Intent intent = new Intent(Intent.ACTION_VIEW);
+    intent.putExtra(QuestionnaireActivity.QUESTIONNAIRE_PARCEL_IDENTIFIER, questionnaire);
+
+    questionnaireActivity = Robolectric.buildActivity(QuestionnaireActivity.class).withIntent(intent).create().get();
+  }
+
+  @Test
+  public void testQuestionnaireParcelIdentifier() {
+    String questionnaireParcelIdentifier = QuestionnaireActivity.QUESTIONNAIRE_PARCEL_IDENTIFIER;
+
+    Assert.assertNotNull(questionnaireParcelIdentifier);
   }
 
   @Test
   public void testQuestionTextViewExists() {
-
     Assert.assertTrue(questionnaireActivity.findViewById(R.id.questionnaire_question_text) instanceof TextView);
-
   }
 
   @Test
   public void testQuestionYesButtonExists() {
-
-    Assert.assertTrue(questionnaireActivity.findViewById(R.id.questionnaire_answer_button_yes) instanceof TextView);
-
+    Assert.assertTrue(questionnaireActivity.findViewById(R.id.questionnaire_answer_button_yes) instanceof Button);
   }
 
   @Test
   public void testQuestionNoButtonExists() {
-
-    Assert.assertTrue(questionnaireActivity.findViewById(R.id.questionnaire_answer_button_no) instanceof TextView);
-
+    Assert.assertTrue(questionnaireActivity.findViewById(R.id.questionnaire_answer_button_no) instanceof Button);
   }
 
+  @Test
+  public void testTextViewSetGivenQuestion() {
+    TextView questionText = (TextView) questionnaireActivity.findViewById(R.id.questionnaire_question_text);
+
+    String expected = questionnaire.getQuestions().get(0).getQuestion();
+
+    Assert.assertEquals(expected, questionText.getText());
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testNoIntentSentToActivity() {
+    questionnaireActivity = Robolectric.buildActivity(QuestionnaireActivity.class).create().get();
+  }
 
 }
