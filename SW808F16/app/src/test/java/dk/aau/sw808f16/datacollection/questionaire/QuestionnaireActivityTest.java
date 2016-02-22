410ff4e2c8f45d869d910397a20347862de5709c
Rasmus Holm Jensen <rhje12@student.aau.dk>
2016-02-22 09:39:05 +0100

    Move QuestionnaireActivityTest to dk.aau.sw808f16.datacollection.questionaire package.

--- SW808F16/app/src/test/java/dk/aau/sw808f16/datacollection/questionaire/QuestionnaireActivityTest.java
+++ SW808F16/app/src/test/java/dk/aau/sw808f16/datacollection/questionaire/QuestionnaireActivityTest.java
@@ -1,8 +1,11 @@
-package dk.aau.sw808f16.sw808f16;
+package dk.aau.sw808f16.datacollection.questionaire;
+
 
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
+
+import dk.aau.sw808f16.datacollection.BuildConfig;
 
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = 21)
