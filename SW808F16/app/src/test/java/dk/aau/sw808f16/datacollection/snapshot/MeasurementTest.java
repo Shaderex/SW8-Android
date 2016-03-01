b774eac0034f560c184da722d41057d51ea60cbb
feupeu <nkmouritzsen@gmail.com>
2016-03-01 14:17:04 +0100

    Create Measurement class

---
+++ SW808F16/app/src/test/java/dk/aau/sw808f16/datacollection/snapshot/MeasurementTest.java
@@ -0,0 +1,12 @@
+package dk.aau.sw808f16.datacollection.snapshot;
+
+import org.junit.Test;
+
+public class MeasurementTest {
+
+  @Test
+  public void testConstructor() {
+    new Measurement();
+  }
+
+}
