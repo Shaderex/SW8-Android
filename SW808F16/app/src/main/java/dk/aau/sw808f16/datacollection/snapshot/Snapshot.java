275b6e3cedaece7f5b3e35ed35e4eaff28b0fcd8
feupeu <nkmouritzsen@gmail.com>
2016-03-02 09:14:36 +0100

    Add samples to Snapshot

--- SW808F16/app/src/main/java/dk/aau/sw808f16/datacollection/snapshot/Snapshot.java
+++ SW808F16/app/src/main/java/dk/aau/sw808f16/datacollection/snapshot/Snapshot.java
@@ -1,10 +1,18 @@
 package dk.aau.sw808f16.datacollection.snapshot;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import dk.aau.sw808f16.datacollection.label.Label;
 
 public class Snapshot {
 
   private Label label;
+  private List<Sample> samples;
+
+  public Snapshot() {
+    this.samples = new ArrayList<>();
+  }
 
   public Label getLabel() {
     return label;
@@ -12,5 +20,23 @@ public class Snapshot {
 
   public void setLabel(Label label) {
     this.label = label;
+  }
+
+  public List<Sample> getSamples() {
+    return samples;
+  }
+
+  public void setSamples(final List<Sample> samples) {
+    this.samples = samples;
+  }
+
+  public void addSamples(final List<Sample> samples) {
+    for (final Sample sample : samples) {
+      addSample(sample);
+    }
+  }
+
+  public void addSample(Sample samples) {
+    this.samples.add(samples);
   }
 }
--- SW808F16/app/src/test/java/dk/aau/sw808f16/datacollection/snapshot/SnapshotTest.java
+++ SW808F16/app/src/test/java/dk/aau/sw808f16/datacollection/snapshot/SnapshotTest.java
@@ -4,6 +4,11 @@ import junit.framework.Assert;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import dk.aau.sw808f16.datacollection.label.Label;
+
 public class SnapshotTest {
 
   @Test
@@ -20,4 +25,47 @@ public class SnapshotTest {
 
     Assert.assertEquals(expectedLabel, snapshot.getLabel());
   }
+
+  @Test
+  public void testGetSetSample() {
+    final Snapshot snapshot = new Snapshot();
+    final Sample sample1 = new Sample();
+    final Sample sample2 = new Sample();
+
+    final List<Sample> samples = new ArrayList<>();
+    samples.add(sample1);
+    samples.add(sample2);
+
+    snapshot.setSamples(samples);
+
+    Assert.assertEquals(snapshot.getSamples(), samples);
+  }
+
+  @Test
+  public void testAddSample() {
+    final Snapshot snapshot = new Snapshot();
+    final Sample sample = new Sample();
+
+    snapshot.addSample(sample);
+
+    Assert.assertTrue(snapshot.getSamples().contains(sample));
+  }
+
+  @Test
+  public void testAddSamples() {
+    final Snapshot snapshot = new Snapshot();
+    final Sample sample1 = new Sample();
+    final Sample sample2 = new Sample();
+
+    final List<Sample> samples = new ArrayList<>();
+    samples.add(sample1);
+    samples.add(sample2);
+
+    snapshot.addSamples(samples);
+
+    for (final Sample sample : samples) {
+      Assert.assertTrue(snapshot.getSamples().contains(sample));
+    }
+  }
+
 }
