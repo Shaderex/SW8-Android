a9d52960fc17bdb0c5a1f3dbcb8d372fdc599d8b
Rasmus Holm Jensen <rhje12@student.aau.dk>
2016-03-04 14:33:04 +0100

    Make cellular sensor provider produce multiple measurements per sample bound to previous test committed at b74975fe1d28cb028e32d97250fcbb2df151968c

--- SW808F16/app/src/main/java/dk/aau/sw808f16/datacollection/backgroundservice/sensorproviders/CellularNetworkSensorProvider.java
+++ SW808F16/app/src/main/java/dk/aau/sw808f16/datacollection/backgroundservice/sensorproviders/CellularNetworkSensorProvider.java
@@ -6,28 +6,55 @@ import android.telephony.CellInfo;
 import android.telephony.TelephonyManager;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 
+import dk.aau.sw808f16.datacollection.R;
 import dk.aau.sw808f16.datacollection.snapshot.Sample;
 
 public class CellularNetworkSensorProvider extends SensorProvider<Sample> {
 
   private final Context context;
+  private final Timer cellNetworkMeasurementTimer;
+
 
   public CellularNetworkSensorProvider(Context context, ExecutorService sensorThreadPool, SensorManager sensorManager) {
     super(context, sensorThreadPool, sensorManager);
     this.context = context;
+    cellNetworkMeasurementTimer = new Timer(true);
   }
 
   @Override
   protected Sample retrieveSampleForDuration(final long sampleDuration, final int measurementFrequency)
       throws InterruptedException {
 
+    final long endTime = System.currentTimeMillis() + sampleDuration;
+    final CountDownLatch latch = new CountDownLatch(1);
     final TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-    final List<CellInfo> cellInfoList = telephonyManager.getAllCellInfo();
+    final List<List<CellInfo>> cellInfoListMeasurements = new ArrayList<>();
 
-    return new Sample(Arrays.asList(cellInfoList));
+    final TimerTask cellNetworkMeasurementTask = new TimerTask() {
+      @Override
+      public void run() {
+        if (System.currentTimeMillis() > endTime) {
+          this.cancel();
+          latch.countDown();
+          return;
+        }
+
+        // Do the measurements
+        cellInfoListMeasurements.add(telephonyManager.getAllCellInfo());
+      }
+    };
+
+    final int micro_per_milli = context.getResources().getInteger(R.integer.micro_seconds_per_milli_second);
+    cellNetworkMeasurementTimer.scheduleAtFixedRate(cellNetworkMeasurementTask, 0, measurementFrequency / micro_per_milli);
+
+    latch.await();
+
+    return new Sample(cellInfoListMeasurements);
   }
 }
