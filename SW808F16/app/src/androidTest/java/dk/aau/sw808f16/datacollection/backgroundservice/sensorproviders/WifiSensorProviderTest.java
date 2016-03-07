4ef0f30f887c578d9a09c2c3b93462c41db9d037
Rasmus Holm Jensen <rhje12@student.aau.dk>
2016-03-07 12:28:06 +0100

    Make wifi sensor provider produce multiple measurements per sample

--- SW808F16/app/src/androidTest/java/dk/aau/sw808f16/datacollection/backgroundservice/sensorproviders/WifiSensorProviderTest.java
+++ SW808F16/app/src/androidTest/java/dk/aau/sw808f16/datacollection/backgroundservice/sensorproviders/WifiSensorProviderTest.java
@@ -10,14 +10,24 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import dk.aau.sw808f16.datacollection.DataCollectionApplication;
+import dk.aau.sw808f16.datacollection.R;
+import dk.aau.sw808f16.datacollection.snapshot.Sample;
 
 public class WifiSensorProviderTest extends ApplicationTestCase<DataCollectionApplication> {
+  private static final long sampleDuration = 10000; // In milliseconds
+  private static final int measurementFrequency = 2000000; // In microseconds
 
-  private static final long sampleDuration = 0; // In milliseconds
-  private static final int measurementFrequency = 0; // In microseconds
+  // Because of real time issues the size may differ +/- 1
+  private int minSize;
+  private int maxSize;
 
-  // GPS will always return exactly one GPS
-  private static final int expectedSize = 1;
+  @Override
+  protected void setUp() throws Exception {
+    final int microPerMilli = this.getContext().getResources().getInteger(R.integer.micro_seconds_per_milli_second);
+    final int expectedSize = (int) (microPerMilli * sampleDuration / measurementFrequency);
+    minSize = expectedSize - 1;
+    maxSize = expectedSize + 1;
+  }
 
   public WifiSensorProviderTest() {
     super(DataCollectionApplication.class);
@@ -28,17 +38,43 @@ public class WifiSensorProviderTest extends ApplicationTestCase<DataCollectionAp
     final SensorManager sensorManager = (SensorManager) getContext().getSystemService(Context.SENSOR_SERVICE);
     final WifiSensorProvider wifiSensorProvider = new WifiSensorProvider(getContext(), sensorThreadPool, sensorManager);
 
-    final List<List<ScanResult>> data1 = wifiSensorProvider.retrieveSampleForDuration(sampleDuration, measurementFrequency);
+    final Sample sample1 = wifiSensorProvider.retrieveSampleForDuration(sampleDuration, measurementFrequency);
 
-    final List<List<ScanResult>> data2 = wifiSensorProvider.retrieveSampleForDuration(sampleDuration, measurementFrequency);
+    final Sample sample2 = wifiSensorProvider.retrieveSampleForDuration(sampleDuration, measurementFrequency);
 
-    assertNotNull("Sensor data is null", data1);
-    assertFalse("Sensor data is empty", data1.isEmpty());
+    assertNotNull("Sensor data is null", sample1);
+    assertFalse("Sensor data is empty", sample1.getMeasurements().isEmpty());
 
-    assertNotNull("Sensor data (second measure) is null", data2);
-    assertFalse("Sensor data (second measure) is empty", data2.isEmpty());
+    assertNotNull("Sensor data (second measure) is null", sample2);
+    assertFalse("Sensor data (second measure) is empty", sample2.getMeasurements().isEmpty());
 
-    assertTrue("The amount of data and sampling period do not match, they are not exactly one", data1.size() == expectedSize);
-    assertTrue("The amount of data and sampling period do not match, they are not exactly one", data2.size() == expectedSize);
+    for (final Object measurement : sample1.getMeasurements()) {
+
+      assertTrue("sample1 is not of type List", measurement instanceof List);
+
+      List<?> aids = (List<?>) measurement;
+
+      for (Object o : aids) {
+        assertTrue("Item in sample1 is not of type ScanResult", o instanceof ScanResult);
+      }
+    }
+
+    for (final Object measurement : sample2.getMeasurements()) {
+
+      assertTrue("sample2 is not of type List", measurement instanceof List);
+
+      List<?> aids = (List<?>) measurement;
+
+      for (Object o : aids) {
+        assertTrue("Item in sample2 is not of type ScanResult", o instanceof ScanResult);
+      }
+    }
+
+    assertTrue("The amount of data and sampling period do not match, not enough data", sample1.getMeasurements().size() >= minSize);
+    assertTrue("The amount of data and sampling period do not match, too much data", sample1.getMeasurements().size() <= maxSize);
+    assertTrue("The amount of data and sampling period do not match, not enough data (second measure)",
+        sample2.getMeasurements().size() >= minSize);
+    assertTrue("The amount of data and sampling period do not match, too much data (second measure)",
+        sample2.getMeasurements().size() <= maxSize);
   }
 }
