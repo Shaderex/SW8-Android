a5115af8363b464d5aea30d611a748ede063f521
feupeu <nkmouritzsen@gmail.com>
2016-03-15 13:15:45 +0100

    Add SnapshotSavedPersistentlySizeTest to test if we have compressed enough data

---
+++ SW808F16/app/src/androidTest/java/dk/aau/sw808f16/datacollection/backgroundservice/SnapshotSavedPersistentlySizeTest.java
@@ -0,0 +1,13 @@
+package dk.aau.sw808f16.datacollection.backgroundservice;
+
+import android.test.ApplicationTestCase;
+
+import dk.aau.sw808f16.datacollection.DataCollectionApplication;
+
+public class SnapshotSavedPersistentlySizeTest extends ApplicationTestCase<DataCollectionApplication> {
+
+  public SnapshotSavedPersistentlySizeTest(Class<DataCollectionApplication> applicationClass) {
+    super(applicationClass);
+  }
+
+}
