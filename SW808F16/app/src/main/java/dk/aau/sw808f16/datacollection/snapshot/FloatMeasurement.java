25dc3915e61eb82196cc93c476aa140a1852a20a
Rasmus Holm Jensen <rhje12@student.aau.dk>
2016-03-30 15:12:32 +0200

    Add FloatMeasurement to store float values on the device

---
+++ SW808F16/app/src/main/java/dk/aau/sw808f16/datacollection/snapshot/FloatMeasurement.java
@@ -0,0 +1,32 @@
+package dk.aau.sw808f16.datacollection.snapshot;
+
+import io.realm.RealmObject;
+
+public class FloatMeasurement extends RealmObject {
+
+  private float value;
+
+  public FloatMeasurement() {
+  }
+
+  public FloatMeasurement(final float value) {
+    this.value = value;
+  }
+
+  public FloatMeasurement(final Float value) {
+    this.value = value;
+  }
+
+  public float getValue() {
+    return value;
+  }
+
+  public void setValue(float value) {
+    this.value = value;
+  }
+
+  @Override
+  public boolean equals(final Object object) {
+    return super.equals(object) || (object instanceof FloatMeasurement && ((FloatMeasurement) object).value == this.value);
+  }
+}
