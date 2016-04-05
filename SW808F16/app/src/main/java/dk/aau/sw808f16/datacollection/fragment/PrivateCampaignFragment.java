6614a42510b51d67f1d6c0071a899c3aecc1aa8a
Jesper Clausen <j.clausen.1992@gmail.com>
2016-04-05 15:23:11 +0200

    Add AsyncHttpWebbTask and use it in private campaign fragment

--- SW808F16/app/src/main/java/dk/aau/sw808f16/datacollection/fragment/PrivateCampaignFragment.java
+++ SW808F16/app/src/main/java/dk/aau/sw808f16/datacollection/fragment/PrivateCampaignFragment.java
@@ -8,8 +8,22 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.goebl.david.Request;
+import com.goebl.david.Response;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.google.android.gms.iid.InstanceID;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 
 import dk.aau.sw808f16.datacollection.R;
+import dk.aau.sw808f16.datacollection.WebUtil.AsyncHttpWebbTask;
 
 public class PrivateCampaignFragment extends Fragment {
 
@@ -48,6 +62,63 @@ public class PrivateCampaignFragment extends Fragment {
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    return inflater.inflate(R.layout.fragment_private_campaign, container, false);
+
+    final View view = inflater.inflate(R.layout.fragment_private_campaign, container, false);
+    final Button submitBtn = (Button) view.findViewById(R.id.private_campaign_join_button);
+    final EditText campaignIdField = (EditText) view.findViewById(R.id.private_campaign_edit_text);
+
+    submitBtn.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Toast.makeText(getActivity(), "Starter", Toast.LENGTH_SHORT).show();
+
+        AsyncHttpWebbTask<JSONObject> task = new AsyncHttpWebbTask<JSONObject>(AsyncHttpWebbTask.Method.POST, "https://dev.local.element67.dk:8000/campaigns/join", 200) {
+          @Override
+          protected Response<JSONObject> sendRequest(Request request) {
+
+            try {
+              final InstanceID instanceID = InstanceID.getInstance(getActivity());
+              final String token = instanceID.getToken(
+                  getString(R.string.defaultSenderID),
+                  GoogleCloudMessaging.INSTANCE_ID_SCOPE,
+                  null
+              );
+              return request
+                  .param("device_id", token)
+                  .param("campaign_id", campaignIdField.getText().toString())
+                  .retry(2, false)
+                  .asJsonObject();
+
+            } catch (IOException e) {
+              e.printStackTrace();
+              return null;
+            }
+          }
+
+          @Override
+          public void onResponseCodeMatching(Response<JSONObject> response) {
+            try {
+              Toast.makeText(getActivity(), response.getBody().getString("message"), Toast.LENGTH_SHORT).show();
+            } catch (JSONException e) {
+              e.printStackTrace();
+            }
+          }
+
+          @Override
+          public void onResponseCodeNotMatching(Response<JSONObject> response) {
+            Toast.makeText(getActivity(), response.getErrorBody().toString(), Toast.LENGTH_SHORT).show();
+          }
+
+          @Override
+          public void onConnectionFailure() {
+            Toast.makeText(getActivity(), "Could not connect", Toast.LENGTH_SHORT).show();
+          }
+        };
+
+        task.execute();
+      }
+    });
+
+    return view;
   }
 }
