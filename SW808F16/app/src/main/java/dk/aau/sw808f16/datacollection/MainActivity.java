c06147cd7e8648763f06ce10f2e63ebef43ca8d5
feupeu <nkmouritzsen@gmail.com>
2016-04-25 15:40:03 +0200

    Add SensorProvider for HeartRate sensor (band2)

--- SW808F16/app/src/main/java/dk/aau/sw808f16/datacollection/MainActivity.java
+++ SW808F16/app/src/main/java/dk/aau/sw808f16/datacollection/MainActivity.java
@@ -2,12 +2,22 @@ package dk.aau.sw808f16.datacollection;
 
 import android.app.Activity;
 import android.app.FragmentManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+
+import com.microsoft.band.BandClient;
+import com.microsoft.band.BandClientManager;
+import com.microsoft.band.BandException;
+import com.microsoft.band.BandInfo;
+import com.microsoft.band.ConnectionState;
+import com.microsoft.band.UserConsent;
+import com.microsoft.band.sensors.HeartRateConsentListener;
 
 import dk.aau.sw808f16.datacollection.fragment.StartFragment;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements HeartRateConsentListener {
 
   private static final String START_FRAGMENT_KEY = "START_FRAGMENT_KEY";
 
@@ -20,6 +30,28 @@ public class MainActivity extends Activity {
     final FragmentManager fragmentManager = getFragmentManager();
 
     fragmentManager.beginTransaction().replace(R.id.content_frame_layout, StartFragment.newInstance(), START_FRAGMENT_KEY).commit();
+
+    Thread getConsent = new Thread(new Runnable() {
+      @Override
+      public void run() {
+        try {
+          getConnectedBandClient();
+
+          if (bandClient != null && bandClient.getSensorManager().getCurrentHeartRateConsent() != UserConsent.GRANTED) {
+            // user has not consented, request consent
+            // the calling class is an Activity and implements
+            // HeartRateConsentListener
+            bandClient.getSensorManager().requestHeartRateConsent(MainActivity.this, MainActivity.this);
+          }
+
+          bandClient = null;
+        } catch (InterruptedException | BandException exception) {
+          exception.printStackTrace();
+        }
+      }
+    });
+    getConsent.start();
+
   }
 
   @Override
@@ -27,5 +59,32 @@ public class MainActivity extends Activity {
     super.onCreateOptionsMenu(menu);
 
     return true;
+  }
+
+  @Override
+  public void userAccepted(boolean accepted) {
+    // handle user's heart rate consent decision
+  }
+
+  private BandClient bandClient;
+
+  protected boolean getConnectedBandClient() throws InterruptedException, BandException {
+    if (bandClient == null) {
+      BandInfo[] devices = BandClientManager.getInstance().getPairedBands();
+      if (devices.length == 0) {
+        Log.d("Band2", "Band isn't paired with your phone (from " + this.getClass().getName() + ").");
+        return false;
+      }
+      bandClient = BandClientManager.getInstance().create(this, devices[0]);
+    } else if (ConnectionState.CONNECTED == bandClient.getConnectionState()) {
+      return true;
+    }
+
+    Log.d("Band2", "Band is connecting...  (from " + this.getClass().getName() + ")");
+    final ConnectionState result = bandClient.connect().await();
+
+    Log.d("Band2", "Band connection status: " + result + " (from " + this.getClass().getName() + ")");
+
+    return ConnectionState.CONNECTED == result;
   }
 }
--- SW808F16/app/src/main/java/dk/aau/sw808f16/datacollection/backgroundservice/sensorproviders/HeartbeatSensorProviderBand.java
+++ SW808F16/app/src/main/java/dk/aau/sw808f16/datacollection/backgroundservice/sensorproviders/HeartbeatSensorProviderBand.java
@@ -1,4 +1,59 @@
 package dk.aau.sw808f16.datacollection.backgroundservice.sensorproviders;
 
-public class HeartbeatSensorProviderBand {
+import android.content.Context;
+import android.hardware.SensorManager;
+
+import com.microsoft.band.BandException;
+import com.microsoft.band.BandIOException;
+import com.microsoft.band.sensors.BandHeartRateEvent;
+import com.microsoft.band.sensors.BandHeartRateEventListener;
+
+import java.util.concurrent.ExecutorService;
+
+import dk.aau.sw808f16.datacollection.snapshot.measurement.HeartRateMeasurement;
+
+public class HeartbeatSensorProviderBand extends SensorProviderBand<HeartRateMeasurement> {
+  public HeartbeatSensorProviderBand(final Context context, final ExecutorService sensorThreadPool, final SensorManager sensorManager) {
+    super(context, sensorThreadPool, sensorManager);
+  }
+
+  @Override
+  protected EventListenerRegistrationManager createRegManager() {
+    final BandHeartRateEventListener listener = new BandHeartRateEventListener() {
+      @Override
+      public void onBandHeartRateChanged(BandHeartRateEvent event) {
+        if (event != null) {
+          onNewMeasurement(new HeartRateMeasurement(event.getHeartRate(), event.getQuality()));
+        }
+      }
+    };
+
+    try {
+      getConnectedBandClient();
+    } catch (InterruptedException | BandException exception) {
+      exception.printStackTrace();
+    }
+
+    return new EventListenerRegistrationManager() {
+      @Override
+      public void register(int frequency) {
+        try {
+          // SampleRate describes the accuracy of the measurement (from Band2)
+          bandClient.getSensorManager().registerHeartRateEventListener(listener);
+        } catch (BandException exception) {
+          exception.printStackTrace();
+        }
+      }
+
+      @Override
+      public void unregister() {
+        try {
+          bandClient.getSensorManager().unregisterHeartRateEventListener(listener);
+        } catch (BandIOException exception) {
+          exception.printStackTrace();
+        }
+      }
+    };
+  }
 }
+
