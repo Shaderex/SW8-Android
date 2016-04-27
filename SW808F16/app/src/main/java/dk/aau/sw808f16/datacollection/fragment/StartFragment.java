9c7ae31e93cb04024e5e8ba6a9bc40bef84b9750
feupeu <nkmouritzsen@gmail.com>
2016-04-27 13:36:08 +0200

    Remove unused code

--- SW808F16/app/src/main/java/dk/aau/sw808f16/datacollection/fragment/StartFragment.java
+++ SW808F16/app/src/main/java/dk/aau/sw808f16/datacollection/fragment/StartFragment.java
@@ -11,25 +11,16 @@ import dk.aau.sw808f16.datacollection.R;
 public class StartFragment extends Fragment {
 
   public static StartFragment newInstance() {
-
     final StartFragment newFragment = new StartFragment();
-
-    /*
-        Bundle args = new Bundle();
-        args.putInt(DRAWABLE_RESOURCE_ID_TAG, drawableResourcesId);
-        newFragment.setArguments(args);
-    */
 
     return newFragment;
   }
 
   @Override
   public View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
-
     final View view = inflater.inflate(R.layout.fragment_start, container, false);
 
     return view;
-
   }
 
 }
