21ff82a755a8806029fe53f5f0971b53be8c7d9a
feupeu <nkmouritzsen@gmail.com>
2016-04-28 15:06:02 +0200

    Refactor the campaign specification view

--- SW808F16/app/src/main/java/dk/aau/sw808f16/datacollection/SensorType.java
+++ SW808F16/app/src/main/java/dk/aau/sw808f16/datacollection/SensorType.java
@@ -2,24 +2,39 @@ package dk.aau.sw808f16.datacollection;
 
 public enum SensorType {
 
-  ACCELEROMETER(0),
-  AMBIENT_LIGHT(1),
-  BAROMETER(2),
-  CELLULAR(3),
-  COMPASS(4),
-  GYROSCOPE(5),
-  LOCATION(6),
-  PROXIMITY(7),
-  WIFI(8);
+  ACCELEROMETER(0, "Accelerometer", SensorCategory.MOVEMENT),
+  AMBIENT_LIGHT(1, "Ambient Light", SensorCategory.MISC),
+  BAROMETER(2, "Barometer", SensorCategory.LOCATION),
+  CELLULAR(3, "Cellular Network", SensorCategory.LOCATION),
+  COMPASS(4, "Compass", SensorCategory.LOCATION),
+  GYROSCOPE(5, "Gyroscope", SensorCategory.MOVEMENT),
+  LOCATION(6, "GPS", SensorCategory.LOCATION),
+  PROXIMITY(7, "PROXIMITY", SensorCategory.MISC),
+  WIFI(8, "Wi-Fi", SensorCategory.LOCATION);
+
+  public enum SensorCategory {
+    LOCATION,
+    MOVEMENT,
+    PERSONAL_INFORMATION,
+    MISC
+  }
 
   private final int identifier;
+  private final String name;
+  private final SensorCategory category;
 
-  SensorType(final int id) {
+  SensorType(final int id, final String name, final SensorCategory category) {
     this.identifier = id;
+    this.name = name;
+    this.category = category;
   }
 
   public int getIdentifier() {
     return identifier;
+  }
+
+  public SensorCategory getCategory() {
+    return category;
   }
 
   public static SensorType getSensorTypeById(final int id) {
@@ -32,5 +47,9 @@ public enum SensorType {
     return null;
   }
 
+  @Override
+  public String toString() {
+    return name;
+  }
 }
 
--- SW808F16/app/src/main/java/dk/aau/sw808f16/datacollection/fragment/CampaignSpecificationFragment.java
+++ SW808F16/app/src/main/java/dk/aau/sw808f16/datacollection/fragment/CampaignSpecificationFragment.java
@@ -3,25 +3,30 @@ package dk.aau.sw808f16.datacollection.fragment;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.v4.widget.ContentLoadingProgressBar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.goebl.david.Response;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.net.HttpURLConnection;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import dk.aau.sw808f16.datacollection.R;
+import dk.aau.sw808f16.datacollection.SensorType;
 import dk.aau.sw808f16.datacollection.campaign.AsyncHttpGetCampaignSpecificationTask;
 import dk.aau.sw808f16.datacollection.campaign.Campaign;
 
@@ -33,7 +38,6 @@ public class CampaignSpecificationFragment extends Fragment {
   public static final String CAMPAIGN_JSON_TAG = "CAMPAIGN_JSON_TAG";
 
   public static CampaignSpecificationFragment newInstance(final long campaignID) {
-
     final CampaignSpecificationFragment newFragment = new CampaignSpecificationFragment();
 
     final Bundle args = new Bundle();
@@ -44,7 +48,6 @@ public class CampaignSpecificationFragment extends Fragment {
   }
 
   public static CampaignSpecificationFragment newInstance(final Campaign campaign) {
-
     final CampaignSpecificationFragment newFragment = new CampaignSpecificationFragment();
     final Bundle args = new Bundle();
 
@@ -61,6 +64,7 @@ public class CampaignSpecificationFragment extends Fragment {
   @Override
   public void onCreate(final Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
     setHasOptionsMenu(true);
   }
 
@@ -76,166 +80,18 @@ public class CampaignSpecificationFragment extends Fragment {
     return super.onOptionsItemSelected(item);
   }
 
-  private static class CampaignSpecificationListingsAdapter extends BaseAdapter {
+  private void updateHeadline(final View parent, final int headlineResourceIdentifier, final String headline, final String description) {
+    final LinearLayout headlineView = (LinearLayout) parent.findViewById(headlineResourceIdentifier);
 
-    JSONArray namesArray = new JSONArray();
-    JSONArray values = new JSONArray();
+    final TextView headlineTextView = (TextView) headlineView.findViewById(R.id.specification_headline);
+    headlineTextView.setText(headline);
 
-    @Override
-    public int getCount() {
-      return namesArray.length();
-    }
-
-    @Override
-    public Object getItem(final int position) {
-      try {
-        return namesArray.getString(position);
-      } catch (JSONException e) {
-        e.printStackTrace();
-      }
-
-      return null;
-    }
-
-    @Override
-    public long getItemId(int position) {
-      return 0;
-    }
-
-    @Override
-    public boolean hasStableIds() {
-      return false;
-    }
-
-    class ViewHolder {
-      TextView campaignSpecificationItemTitle;
-      TextView campaignSpecificationItemValue;
-      TextView campaignSpecificationItemLargeTextBlock;
-    }
-
-    @Override
-    public View getView(final int position, View convertView, final ViewGroup parent) {
-
-      if (convertView == null) {
-        final LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-
-        convertView = inflater.inflate(R.layout.fragment_campaign_specification_item, null);
-
-        final ViewHolder holder = new ViewHolder();
-
-        holder.campaignSpecificationItemTitle = (TextView) convertView.findViewById(R.id.campaign_specification_item_title);
-        holder.campaignSpecificationItemValue = (TextView) convertView.findViewById(R.id.campaign_specification_item_value);
-        holder.campaignSpecificationItemLargeTextBlock =
-            (TextView) convertView.findViewById(R.id.campaign_specification_item_large_text_block);
-
-        convertView.setTag(holder);
-      }
-
-      final ViewHolder holder = (ViewHolder) convertView.getTag();
-
-      try {
-        String name = namesArray.getString(position);
-
-        // Remove underscores and make first letter uppercase
-        name = name.replace('_', ' ');
-        name = name.substring(0, 1).toUpperCase() + name.substring(1);
-
-        holder.campaignSpecificationItemTitle.setText(name);
-
-        switch (name) {
-          case "Sensors": {
-
-            final JSONArray array = values.getJSONArray(position);
-
-            holder.campaignSpecificationItemValue.setVisibility(View.VISIBLE);
-            holder.campaignSpecificationItemLargeTextBlock.setVisibility(View.GONE);
-
-            final StringBuilder builder = new StringBuilder();
-
-            for (int counter = 0; counter < array.length(); counter++) {
-
-              builder.append(array.getJSONObject(counter).getString("name"));
-
-              if (counter != array.length() - 1) {
-                builder.append('\n');
-              }
-            }
-
-            holder.campaignSpecificationItemValue.setText(builder.toString());
-
-            return convertView;
-          }
-          case "Questions": {
-
-            final JSONArray array = values.getJSONArray(position);
-
-            holder.campaignSpecificationItemValue.setVisibility(View.VISIBLE);
-            holder.campaignSpecificationItemLargeTextBlock.setVisibility(View.GONE);
-
-            final StringBuilder builder = new StringBuilder();
-
-            for (int counter = 0; counter < array.length(); counter++) {
-
-              builder.append(array.getJSONObject(counter).getString("question"));
-
-              if (counter != array.length() - 1) {
-                builder.append('\n');
-              }
-            }
-
-            holder.campaignSpecificationItemValue.setText(builder.toString());
-
-            return convertView;
-          }
-          case "Description": {
-
-            holder.campaignSpecificationItemValue.setVisibility(View.GONE);
-            holder.campaignSpecificationItemLargeTextBlock.setVisibility(View.VISIBLE);
-            holder.campaignSpecificationItemLargeTextBlock.setText(values.getString(position));
-
-            return convertView;
-          }
-          default: {
-
-            holder.campaignSpecificationItemValue.setVisibility(View.VISIBLE);
-            holder.campaignSpecificationItemValue.setText(values.getString(position));
-            holder.campaignSpecificationItemLargeTextBlock.setVisibility(View.GONE);
-
-            /*
-            if (Number.class.isAssignableFrom(values.get(position).getClass())) {
-              holder.campaignSpecificationItemValue.setGravity(Gravity.CENTER_HORIZONTAL);
-            } else {
-              holder.campaignSpecificationItemValue.setGravity(Gravity.START);
-            }*/
-
-            return convertView;
-          }
-        }
-
-      } catch (JSONException exception) {
-        exception.printStackTrace();
-      }
-
-      return null;
-    }
-
-    public void setCampaignSpecification(final JSONObject campaignSpecification) {
-
-      namesArray = campaignSpecification.names();
-
-      try {
-        values = campaignSpecification.toJSONArray(namesArray);
-      } catch (JSONException exception) {
-        exception.printStackTrace();
-      }
-
-      this.notifyDataSetChanged();
-    }
+    final TextView descriptionTextView = (TextView) headlineView.findViewById(R.id.specification_description);
+    descriptionTextView.setText(description);
   }
 
   @Override
   public View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
-
     final Bundle arguments = getArguments();
 
     if (arguments == null) {
@@ -246,20 +102,21 @@ public class CampaignSpecificationFragment extends Fragment {
     final ContentLoadingProgressBar activityIndicator = (ContentLoadingProgressBar) view.findViewById(R.id.activity_indicator);
     final ListView campaignSpecificationListing = (ListView) view.findViewById(R.id.fragment_campaign_specification_listing);
 
-    final CampaignSpecificationListingsAdapter adapter = new CampaignSpecificationListingsAdapter();
-    campaignSpecificationListing.setAdapter(adapter);
+    // Set headlines
+    updateHeadline(view, R.id.information_gathered_headline, "Information Gathered", "The following information will be collected");
+    updateHeadline(view, R.id.measurements_headline, "Measurements", "Will affect the battery consumption");
+    updateHeadline(view, R.id.questions_headline, "Questions", "You will be asked questions such as");
+
     activityIndicator.show();
 
     if (arguments.containsKey(CAMPAIGN_ID_TAG)) {
-      // Start AsyncTask
-
       final long campaignId = arguments.getLong(CAMPAIGN_ID_TAG);
 
       final AsyncHttpGetCampaignSpecificationTask task = new AsyncHttpGetCampaignSpecificationTask(getActivity(), campaignId) {
-
         @Override
         public void onResponseCodeMatching(Response<JSONObject> response) {
-          adapter.setCampaignSpecification(response.getBody());
+          view.setVisibility(View.VISIBLE);
+          updateCampaignSpecification(view, response.getBody());
           activityIndicator.hide();
         }
 
@@ -267,32 +124,34 @@ public class CampaignSpecificationFragment extends Fragment {
         public void onResponseCodeNotMatching(final Response<JSONObject> response) {
           super.onResponseCodeNotMatching(response);
 
-          final TextView textView = (TextView) getView().findViewById(R.id.error_message_textview);
+          view.setVisibility(View.VISIBLE);
+
+          final TextView errorMessageTextView = (TextView) getView().findViewById(R.id.error_message_textview);
 
           activityIndicator.hide();
 
           switch (response.getStatusCode()) {
             case HttpURLConnection.HTTP_NOT_FOUND: {
 
-              textView.setText(R.string.unable_to_locate_campaign_message);
+              errorMessageTextView.setText(R.string.unable_to_locate_campaign_message);
               break;
             }
             default: {
-              textView.setText(R.string.generic_something_went_wrong_message);
+              errorMessageTextView.setText(R.string.generic_something_went_wrong_message);
               break;
             }
           }
 
-          textView.setVisibility(View.VISIBLE);
-
+          errorMessageTextView.setVisibility(View.VISIBLE);
         }
       };
 
+      view.setVisibility(View.INVISIBLE);
       task.execute();
 
     } else if (arguments.containsKey(CAMPAIGN_JSON_TAG)) {
       try {
-        adapter.setCampaignSpecification(new JSONObject(arguments.getString(CAMPAIGN_JSON_TAG)));
+        updateCampaignSpecification(view, new JSONObject(arguments.getString(CAMPAIGN_JSON_TAG)));
         activityIndicator.hide();
       } catch (JSONException exception) {
         exception.printStackTrace();
@@ -302,5 +161,80 @@ public class CampaignSpecificationFragment extends Fragment {
     }
 
     return view;
+  }
+
+  private void updateCampaignSpecification(final View parent, final JSONObject campaignSpecification) {
+    Log.d("data", campaignSpecification.toString());
+
+    try {
+      final Campaign campaign = new Campaign(campaignSpecification);
+
+      final String campaignTitle = campaign.getName();
+      final TextView campaignTitleTextView = (TextView) parent.findViewById(R.id.campaign_specification_title);
+      campaignTitleTextView.setText(campaignTitle);
+
+      final String campaignAuthor = campaignSpecification.getJSONObject("user").getString("name");
+      final TextView campaignAuthorTextView = (TextView) parent.findViewById(R.id.campaign_specification_by_line);
+      campaignAuthorTextView.setText("by " + campaignAuthor);
+
+      final String campaignDescription = campaign.getDescription();
+      final TextView campaignDescriptionTextView = (TextView) parent.findViewById(R.id.campaign_specification_description);
+      campaignDescriptionTextView.setText(campaignDescription);
+
+      HashMap<SensorType.SensorCategory, List<SensorType>> sensorsUsed = new HashMap<>();
+      for (final SensorType sensor : campaign.getSensors()) {
+        if (sensorsUsed.get(sensor.getCategory()) == null) {
+          sensorsUsed.put(sensor.getCategory(), new ArrayList<SensorType>());
+        }
+        sensorsUsed.get(sensor.getCategory()).add(sensor);
+      }
+
+      updateCampaignMeasurementsCategory(parent, R.id.measurement_category_location, R.drawable.ic_room_black_36dp, "Location", sensorsUsed.get(SensorType.SensorCategory.LOCATION));
+      updateCampaignMeasurementsCategory(parent, R.id.measurement_category_movement, R.drawable.ic_directions_run_black_36dp, "Movement", sensorsUsed.get(SensorType.SensorCategory.MOVEMENT));
+      updateCampaignMeasurementsCategory(parent, R.id.measurement_category_personal_information, R.drawable.ic_favorite_black_36dp, "Personal Information", sensorsUsed.get(SensorType.SensorCategory.PERSONAL_INFORMATION));
+
+      final int sampleDuration = campaignSpecification.getInt("sample_duration");
+      final int sampleFrequency = campaignSpecification.getInt("sample_frequency");
+      final int measurementFrequency = campaignSpecification.getInt("measurement_frequency");
+      final int measurementsPerHour = (int) ((double) 3600000 / (double) sampleFrequency * (double) sampleDuration / (double) measurementFrequency);
+
+      final TextView measurementsRateTextView = (TextView) parent.findViewById(R.id.measurements_rate);
+      measurementsRateTextView.setText(measurementsPerHour + " measurements per hour");
+
+    } catch (JSONException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void updateCampaignMeasurementsCategory(final View parent,
+                                                  final int categoryResource,
+                                                  final int indicatorResource,
+                                                  final String headline,
+                                                  final List<SensorType> sensors) {
+
+    final LinearLayout categoryView = (LinearLayout) parent.findViewById(categoryResource);
+
+    // If no description is provided, do not display the view
+    if (sensors == null || sensors.size() == 0) {
+      categoryView.setVisibility(View.GONE);
+      return;
+    }
+
+    final ImageView indicatorImageView = (ImageView) categoryView.findViewById(R.id.category_indicator);
+    indicatorImageView.setImageDrawable(getResources().getDrawable(indicatorResource));
+
+    final TextView headlineTextView = (TextView) categoryView.findViewById(R.id.category_headline);
+    headlineTextView.setText(headline);
+
+    final TextView descriptionTextView = (TextView) categoryView.findViewById(R.id.category_description);
+    String sensorString = "";
+    for (int i = 0; i < sensors.size(); i++) {
+      SensorType type = sensors.get(i);
+      sensorString += type.toString();
+      if(i != sensors.size() - 1) {
+        sensorString += ", ";
+      }
+    }
+    descriptionTextView.setText(sensorString);
   }
 }
